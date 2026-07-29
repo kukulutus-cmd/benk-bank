1a69bca540963a401e66ac288b6b3a9884f1ec18
kukulutus-cmd <putrainti.sagara@gmail.com>
2026-07-30 02:12:19 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 
 export const APP_CONFIG = {
   // Ganti URL di bawah ini dengan URL Web App dari Google Apps Script Anda setelah deployment
-  GAS_WEB_APP_URL: "MASUKKAN_URL_WEB_APP_GAS_ANDA_DISINI",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw0westzogqzY3FkXWEKj5kv8I6sADOmMB__85S-6bzhzdN-WmuUw4QBsK6fLfIfp_q/exec",
   
   // App metadata
   APP_NAME: "Bank Daily Unit Reporting Web App",
